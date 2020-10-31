a2eb3598239dc68d5835ee69976e8b2fb764a9ab
Rs1124 <604347974@qq.com>
2020-10-31 21:46:54 +0800

    二叉树_更新

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,19 +1,23 @@
-/*二叉树的链式存储__by递归实现
-前序遍历
-中序遍历
-后序遍历*/ 
+/*二叉树的链式存储*/ 
 #include<bits/stdc++.h>
 using namespace std;
 
 typedef struct BiTNode{
 	char data;
-	struct BiTNode *lchild,*rchild; //lchild 左孩子指针,rchild 右孩子指针 
+	struct BiTNode *lchild,*rchild; //lchild:左孩子指针；rchild:右孩子指针 
+	int ltag,rtag; //ltag:前驱线索；rtag:后继线索 
 }BiTNode,*BiTree; //BiTNode 用于强调构建树的节点，BiTree 用于强调整个树 
+
+BiTNode *pre=NULL; //用于前驱指针
 
 void create_tree(BiTree &T); //二叉树的创建（通过递归实现） 
 void pr_tra(BiTree &T); //前序遍历
 void in_tra(BiTree &T); //中序遍历
 void po_tra(BiTree &T); //后序遍历 
+void vist(BiTNode *q); //具体的线索化操作 
+void pr_thread(BiTree &T); //前序线索化 
+void in_thread(BiTree &T); //中序线索化 
+void po_thread(BiTree &T); //后序线索化 
 
 void create_tree(BiTree &T){ 
 	char ch;
@@ -50,8 +54,41 @@ void po_tra(BiTree &T){
 		cout<<T->data;					
 	} 
 }
+void vist(BiTNode *q){
+	if(q->lchild==NULL){ //如果该结点左子树为空,就让该节点的左孩子指向pre指针 
+		q->lchild=pre;
+		q->ltag=1; //如果ltag=1则表示该节点没有左孩子是一个前驱指针，否则表示该节点存在左孩子 
+	}
+	if(pre!=NULL&&pre->rchild==NULL){
+		pre->rchild=q;
+		pre->rtag=1; 
+	} 
+	pre=q; //让pre指针指向q结点 
+}
+void pr_thread(BiTree &T){
+	if(T!=NULL){
+		vist(T);
+		if(T->ltag==0) pr_thread(T->lchild);
+		pr_thread(T->rchild); 
+	}
+} 
+void in_thread(BiTree &T){
+	if(T!=NULL){
+		in_thread(T->lchild);
+		vist(T);
+		in_thread(T->rchild;)	
+	}
+} 
+void po_thread(BiTree &T){
+	if(T!=NULL){
+		in_thread(T->lchild);
+		in_thread(T->rchild);
+		vist(T);
+	}
+}
+
 int main(){
-	BiTree T;
+	BiTree T; 
 	create_tree(T); 
 	cout<<"前序遍历(根左右):";
 	pr_tra(T);
@@ -61,4 +98,6 @@ int main(){
 	cout<<endl;
 	cout<<"后序遍历(左右根):"; 
 	po_tra(T);
+	
+//	if(pre->rchild==NULL) pre->rtag=1;	//已经到了最后一个节点 
 }
